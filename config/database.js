7d5a615e38d91b0c208c4ad60a612cd582c72b5f
dag-bag <virenderkumar23435@gmail.com>
2023-03-27 19:56:13 +0530

    init commit

---
+++ config/database.js
@@ -0,0 +1,27 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "strapi"),
+      user: env("PGUSER", "strapi"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
+    },
+  },
+});
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("PGHOST", "containers-us-west-40.railway.app"),
+//       port: env.int("PGPORT", 7371),
+//       database: env("PGDATABASE", "railway"),
+//       user: env("PGUSER", "postgres"),
+//       password: env("PGPASSWORD", "cto0yj35MQD0kJdelTj0"),
+//       ssl: env.bool(true),
+//     },
+//   },
+// });
---
+++ config/plugins.js
@@ -0,0 +1,63 @@
+module.exports = ({ env }) => ({
+  seo: {
+    enabled: true,
+  },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  io: {
+    enabled: true,
+    config: {
+      IOServerOptions: {
+        cors: { origin: "http://localhost:5000", methods: ["GET"] },
+      },
+      contentTypes: {
+        message: "*",
+        chat: ["create"],
+      },
+      events: [
+        {
+          name: "connection",
+          handler: ({ strapi }, socket) => {
+            strapi.log.info(`[io] new connection with id ${socket.id}`);
+          },
+        },
+      ],
+    },
+  },
+  "entity-relationship-chart": {
+    enabled: true,
+    config: {
+      // By default all contentTypes and components are included.
+      // To exlclude strapi's internal models, use:
+      exclude: [
+        "strapi::core-store",
+        "webhook",
+        "admin::permission",
+        "admin::user",
+        "admin::role",
+        "admin::api-token",
+        "plugin::upload.file",
+        "plugin::i18n.locale",
+        "plugin::users-permissions.permission",
+        "plugin::users-permissions.role",
+      ],
+    },
+  },
+});
